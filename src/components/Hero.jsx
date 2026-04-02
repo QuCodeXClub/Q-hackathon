d31aa9634d315105680cc2985e54e07dc1446712
Tushar Saini <tusharsaini.in@gmail.com>
2026-04-02 21:39:21 +0530

    refactor: simplify mouse movement handling in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,28 +48,7 @@ const Hero = () => {
       transition: { type: "spring", stiffness: 80, damping: 15 } 
     },
   };
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const mouseXSpring = useSpring(x, { stiffness: 150, damping: 15 });
-  const mouseYSpring = useSpring(y, { stiffness: 150, damping: 15 });
-  const rotateX = useTransform(mouseYSpring, [-0.5, 0.5], ["15deg", "-15deg"]);
-  const rotateY = useTransform(mouseXSpring, [-0.5, 0.5], ["-15deg", "15deg"]);
-  const handleMouseMove = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-    const xPct = mouseX / width - 0.5;
-    const yPct = mouseY / height - 0.5;
-    x.set(xPct);
-    y.set(yPct);
-  };
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
-
+  
   //3d hover tilt
   const x = useMotionValue(0);
   const y = useMotionValue(0);
